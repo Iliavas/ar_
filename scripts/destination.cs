7cd630702069e0f297379ff176bb452c1903ce64
ilvsl <il.vsl0110@gmail.com>
2020-03-02 16:31:18 +0300

    ''

---
+++ scripts/destination.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class destination : MonoBehaviour
+{
+    public GameObject arrow;
+    private List<GameObject> arrows = new List<GameObject>();
+    private string target = "Target1";
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        StartCoroutine("Instance");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = GameObject.FindGameObjectWithTag(target).transform.position;
+    }
+
+    public void Change_target(string target) 
+    {
+        this.target = target;
+        destroy();
+        StartCoroutine("Instance");
+    }
+
+    public void destroy()
+    {
+        foreach (GameObject i in arrows) Destroy(i);
+        arrows = new List<GameObject>();
+    }
+
+    IEnumerator Instance()
+    {
+        yield return new WaitForSeconds(0.3f);
+        GameObject obj;
+        obj = Instantiate(arrow, transform.position, Quaternion.identity);
+        arrows.Add(obj);
+        Debug.Log(arrows[0]);
+
+        if (agent.hasPath) StartCoroutine("Instance");
+    }
+}
---
+++ scripts/set_target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class set_target : MonoBehaviour
+{
+    [SerializeField]
+    destination dol;
+
+    private void destr()
+    {
+        
+    }
+    public void Get_target1()
+    {
+        dol.Change_target("Target1");
+    }
+    public void Get_Target2()
+    {
+        dol.Change_target("Target2");
+    }
+}
